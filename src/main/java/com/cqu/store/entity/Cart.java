28dffc39cb81002e7f8e84926f06415a08baa209
liulllll <1597121033@qq.com>
2022-06-20 11:19:35 +0800

    购物车实体类

---
+++ src/main/java/com/cqu/store/entity/Cart.java
@@ -0,0 +1,80 @@
+package com.cqu.store.entity;
+
+import java.util.Objects;
+
+public class Cart extends BaseEntity{
+    private Integer cid;
+    private Integer uid;
+    private Integer pid;
+    private Long price;
+    private Integer num;
+
+    public Integer getCid(){
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "cid=" + cid +
+                ", uid=" + uid +
+                ", pid=" + pid +
+                ", price=" + price +
+                ", num=" + num +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cart)) return false;
+        Cart cart = (Cart) o;
+        return Objects.equals(getCid(), cart.getCid()) &&
+                Objects.equals(getUid(), cart.getUid()) &&
+                Objects.equals(getPid(), cart.getPid()) &&
+                Objects.equals(getPrice(), cart.getPrice()) &&
+                Objects.equals(getNum(), cart.getNum());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getCid(), getUid(), getPid(), getPrice(), getNum());
+    }
+}
