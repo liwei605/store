9d30b2c21f0eafa02c46447c4c924426626ead98
liwei605 <2499280864@qq.com>
2022-06-21 17:34:08 +0800

    李为 张鸿宇  删除所选bug修改

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,20 +1,3 @@
-/*按加号数量增*/
-// function addNum(rid) {
-//     var n = parseInt($("#goodsCount" + rid).val());
-//     $("#goodsCount" + rid).val(n + 1);
-//     calcRow(rid);
-//     calcTotal();
-// }
-
-/*按减号数量减*/
-// function reduceNum(rid) {
-//     var n = parseInt($("#goodsCount" + rid).val());
-//     if (n == 0)
-//         return;
-//     $("#goodsCount" + rid).val(n - 1);
-//     calcRow(rid);
-//     calcTotal();
-// }
 
 /*全选全不选*/
 function checkall(ckbtn) {
@@ -22,47 +5,46 @@ function checkall(ckbtn) {
     calcTotal();
 }
 
-//删除按钮
-// function delCartItem(btn) {
-//
-//     $(btn).parents("tr").remove();
-//     console.log("开始计算价格");
-//     calcTotal();
-// }
-
 //批量删除按钮
 function selDelCart() {
     //遍历所有按钮
-    for (var i = $(".ckitem").length - 1; i >= 0; i--) {
+	var i =$(".ckitem").length - 1;
+	var flag=0;
+    for (i ; i >= 0; i--) {
         //如果选中
         if ($(".ckitem")[i].checked) {
             //删除
-            $($(".ckitem")[i]).parents("tr").remove();
+			var cid=$($(".ckitem")[i]).val();
+			// delCartItem(value);
+			$.ajax({
+				url: "/carts/" + cid + "/num/delete",
+				type: "POST",
+				dataType: "JSON",
+				success: function (json) {
+					if (json.state == 200) {
+						//alert("删除成功！");
+
+					} else {
+						flag=-1;
+					}
+				},
+				error: function (xhr) {
+					alert("您的登录信息已经过期，请重新登录！HTTP响应码：" + xhr.status);
+					location.href = "login.html";
+				}
+			});
+			if(flag==0)
+			{
+				$($(".ckitem")[i]).parents("tr").remove();
+			}else
+			{
+				alert("所选中的商品删除失败！");
+			}
         }
     }
     calcTotal();
 }
-//
-// $(function () {
-    //单选一个也得算价格
-    // $(".ckitem").click(function () {
-    //     calcTotal();
-    // })
-    //开始时计算价格
-   // calcTotal();
-// })
 
-//计算单行小计价格的方法
-// function calcRow(rid) {
-// //     //取单价
-// //     var vprice = parseFloat($("#goodsPrice" + rid).html());
-// //     //取数量
-// //     var vnum = parseFloat($("#goodsCount" + rid).val());
-// //     //小计金额
-// //     var vtotal = vprice * vnum;
-// //     //赋值
-// //     $("#goodsCast" + rid).html("¥" + vtotal);
-// // }
 
 //计算总价格的方法
 
