d94e33cb4aa625cc7a5322b0b2716e2c5bed0d5e
liulllll <1597121033@qq.com>
2022-06-21 15:03:26 +0800

    购物车商品增加、减少、删除 14点56分

---
+++ src/test/java/com/cqu/store/service/CartServiceTests.java
@@ -0,0 +1,59 @@
+package com.cqu.store.service;
+
+import com.cqu.store.service.ex.ServiceException;
+import com.cqu.store.vo.CartVO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CartServiceTests {
+    @Autowired
+    private ICartService cartService;
+
+    @Test
+    public void addToCart(){
+        cartService.addToCart(6,10000007,3,"aaa");
+    }
+
+    @Test
+    public void getVOByUid() {
+        List<CartVO> list = cartService.getVOByUid(6);
+        System.out.println("count=" + list.size());
+        for (CartVO item : list) {
+            System.out.println(item);
+        }
+    }
+
+    @Test
+    public void addNum() {
+        try {
+            Integer cid = 6;
+            Integer uid = 6;
+            String username = "管理员";
+            Integer num = cartService.addNum(cid, uid, username);
+            System.out.println("OK. New num=" + num);
+        } catch (ServiceException e) {
+            System.out.println(e.getClass().getSimpleName());
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    public void reduceNum() {
+        try {
+            Integer cid = 6;
+            Integer uid = 6;
+            String username = "管理员";
+            Integer num = cartService.reduceNum(cid, uid, username);
+            System.out.println("OK. New num=" + num);
+        } catch (ServiceException e) {
+            System.out.println(e.getClass().getSimpleName());
+            System.out.println(e.getMessage());
+        }
+    }
+}
