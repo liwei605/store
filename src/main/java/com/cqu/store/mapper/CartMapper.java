d941b42da98dcb1080aebd526003a9d5db61303f
liulllll <1597121033@qq.com>
2022-06-21 14:57:02 +0800

    购物车商品增加、减少、删除 14点56分

--- src/main/java/com/cqu/store/mapper/CartMapper.java
+++ src/main/java/com/cqu/store/mapper/CartMapper.java
@@ -50,4 +50,18 @@ public interface CartMapper {
      * @return 该用户的购物车数据的列表
      */
     List<CartVO> findVOByUid(Integer uid);
+
+    /**
+     * 根据购物车数据id查询购物车数据详情
+     * @param cid 购物车数据id
+     * @return 匹配的购物车数据详情，如果没有匹配的数据则返回null
+     */
+    Cart findByCid(Integer cid);
+
+    /**
+     * 根据购物车数据id删除该商品数据
+     * @param cid 购物车数据id
+     * @return 匹配的购物车数据详情，如果没有匹配的数据则返回null
+     */
+    boolean deleteByCid(Integer cid);
 }
--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,31 +1,34 @@
 /*按加号数量增*/
-function addNum(rid) {
-    var n = parseInt($("#goodsCount" + rid).val());
-    $("#goodsCount" + rid).val(n + 1);
-    calcRow(rid);
-}
+// function addNum(rid) {
+//     var n = parseInt($("#goodsCount" + rid).val());
+//     $("#goodsCount" + rid).val(n + 1);
+//     calcRow(rid);
+//     calcTotal();
+// }
 
 /*按减号数量减*/
-function reduceNum(rid) {
-    var n = parseInt($("#goodsCount" + rid).val());
-    if (n == 0)
-        return;
-    $("#goodsCount" + rid).val(n - 1);
-    calcRow(rid);
-}
+// function reduceNum(rid) {
+//     var n = parseInt($("#goodsCount" + rid).val());
+//     if (n == 0)
+//         return;
+//     $("#goodsCount" + rid).val(n - 1);
+//     calcRow(rid);
+//     calcTotal();
+// }
 
 /*全选全不选*/
 function checkall(ckbtn) {
     $(".ckitem").prop("checked", $(ckbtn).prop("checked"));
-    //calcTotal();
+    calcTotal();
 }
 
 //删除按钮
-function delCartItem(btn) {
-
-    $(btn).parents("tr").remove();
-    //calcTotal();
-}
+// function delCartItem(btn) {
+//
+//     $(btn).parents("tr").remove();
+//     console.log("开始计算价格");
+//     calcTotal();
+// }
 
 //批量删除按钮
 function selDelCart() {
@@ -37,16 +40,16 @@ function selDelCart() {
             $($(".ckitem")[i]).parents("tr").remove();
         }
     }
-    //calcTotal();
+    calcTotal();
 }
 
 $(function () {
     //单选一个也得算价格
     $(".ckitem").click(function () {
-        //calcTotal();
+        calcTotal();
     })
     //开始时计算价格
-    //calcTotal();
+    calcTotal();
 })
 
 //计算单行小计价格的方法
@@ -62,12 +65,12 @@ function calcRow(rid) {
 }
 
 //计算总价格的方法
-/*
+
 function calcTotal() {
 	//选中商品的数量
 	var vselectCount = 0;
 	//选中商品的总价
-	var vselectTotal = 0;
+    var vselectTotal = 0;
 
 	//循环遍历所有tr
 	for (var i = 0; i < $(".cart-body tr").length; i++) {
@@ -95,4 +98,4 @@ function calcTotal() {
 		$("#selectTotal").html(vselectTotal);
 		$("#selectCount").html(vselectCount);
 	}
-}*/
+}
