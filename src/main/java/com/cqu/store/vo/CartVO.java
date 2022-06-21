0281ac87e7962e733e2086c58c8faf99c287b0a0
zhy <1205399630@qq.com>
2022-06-21 10:48:36 +0800

    购物车数据的Value Object类

---
+++ src/main/java/com/cqu/store/vo/CartVO.java
@@ -0,0 +1,110 @@
+package com.cqu.store.vo;
+
+import java.io.Serializable;
+
+/**  购物车数据的Value Object类*/
+public class CartVO implements Serializable {
+    private Integer cid;
+    private Integer uid;
+    private Integer pid;
+    private Long price;
+    private Integer num;
+    private String title;
+    private Long realPrice;
+    private String image;
+
+    @Override
+    public String toString() {
+        return "CartVO{" +
+                "cid=" + cid +
+                ", uid=" + uid +
+                ", pid=" + pid +
+                ", price=" + price +
+                ", num=" + num +
+                ", title='" + title + '\'' +
+                ", realPrice=" + realPrice +
+                ", image='" + image + '\'' +
+                '}';
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getRealPrice() {
+        return realPrice;
+    }
+
+    public void setRealPrice(Long realPrice) {
+        this.realPrice = realPrice;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public CartVO(Integer cid, Integer uid, Integer pid, Long price, Integer num, String title, Long realPrice, String image) {
+
+        this.cid = cid;
+        this.uid = uid;
+        this.pid = pid;
+        this.price = price;
+        this.num = num;
+        this.title = title;
+        this.realPrice = realPrice;
+        this.image = image;
+    }
+
+    public CartVO() {
+
+    }
+// Generate: Getter and Setter??Generate hashCode() and equals()??toString()
+}
